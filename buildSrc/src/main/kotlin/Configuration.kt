2d6d7dacf8e095d5f35121306eb5e938a9e6944c
mx-smblt <mx.smblt@gmail.com>
2023-12-17 14:54:31 +0200

    Optimized imports.

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -1,7 +1,4 @@
 import org.gradle.api.JavaVersion
-import org.gradle.api.Project
-import org.gradle.api.artifacts.dsl.RepositoryHandler
-import org.gradle.api.publish.maven.MavenPom
 
 @Suppress("unused")
 object Configuration {
